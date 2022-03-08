3acf8be6e243e00ca82fc36640692cc411ca4f32
inhajuyoung <wndud4576@naver.com>
2022-03-08 21:28:26 +0900

    Create Database.kt Create DatabaseControl.kt

--- MyApplication/app/src/main/java/com/example/myapplication/Database.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Database.kt
@@ -3,13 +3,12 @@ package com.example.myapplication
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.graphics.BitmapFactory
 
 class Database(context: Context?, name : String, factory: SQLiteDatabase.CursorFactory?, version: Int)
     : SQLiteOpenHelper(context, name, factory, version) {
 
     override fun onCreate(database: SQLiteDatabase?) {
-        val sql = "CREATE TABLE IF NOT EXISTS account(seq INTEGER PRIMARY KEY AUTOINCREMENT, id TEXT, pw TEXT)"
+        val sql = "CREATE TABLE IF NOT EXISTS user(seq INTEGER PRIMARY KEY AUTOINCREMENT, id TEXT, pw TEXT)"
         database!!.execSQL(sql)
     }
 
