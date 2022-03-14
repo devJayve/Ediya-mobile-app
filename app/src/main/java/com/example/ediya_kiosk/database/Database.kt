7b64d806b093387b78158289ff3d862dfb047431
inhajuyoung <wndud4576@naver.com>
2022-03-14 21:52:10 +0900

    Modify DB structure

--- app/src/main/java/com/example/ediya_kiosk/database/Database.kt
+++ app/src/main/java/com/example/ediya_kiosk/database/Database.kt
@@ -9,14 +9,12 @@ class Database(context: Context?, name : String, factory: SQLiteDatabase.CursorF
 
     override fun onCreate(database: SQLiteDatabase?) {
         val accountSql = "CREATE TABLE IF NOT EXISTS account(id TEXT, password TEXT, email TEXT, name TEXT,certification_num TEXT,phone_num TEXT,point INTEGER)"
-        val basketSql = "CREATE TABLE IF NOT EXISTS basket(id TEXT, basket_index INTEGER PRIMARY KEY AUTOINCREMENT, menu_name TEXT, menu_count INTEGER, menu_temp TEXT, menu_size TEXT, menu_price INTEGER, menu_img TEXT, option_cost INTEGER, total_cost INTEGER)"
+        val basketSql = "CREATE TABLE IF NOT EXISTS basket(id TEXT, basket_index INTEGER, menu_name TEXT, menu_count INTEGER, menu_temp TEXT, menu_size TEXT, menu_price INTEGER, menu_img TEXT, option_cost INTEGER, total_cost INTEGER,pay_state Boolean)"
         val interfaceSql = "CREATE TABLE IF NOT EXISTS interface(id TEXT, isMode BOOLEAN, isLanguage BOOLEAN)"
-        val orderSql = "CREATE TABLE IF NOT EXISTS order_history(id TEXT, order_index INTEGER, basket_num INTEGER, menu_name TEXT,menu_state BOOLEAN, menu_count INTEGER, menu_temp TEXT, menu_size TEXT, menu_price INTEGER, option_cost INTEGER, total_cost INTEGER,discount INTEGER, pay_kind INTEGER)"
 
         database!!.execSQL(accountSql)
         database!!.execSQL(basketSql)
         database!!.execSQL(interfaceSql)
-        database!!.execSQL(orderSql)
     }
 
     override fun onUpgrade(database: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
